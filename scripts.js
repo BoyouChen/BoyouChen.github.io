fdd81e02d9aec9115b95d13fe5816f6a6f3db5bf
BoyouChen <114955802+BoyouChen@users.noreply.github.com>
2023-03-27 01:38:21 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+$(document).ready(function () {
+  let words = $('.word');
+  let currentIndex = 0;
+  let nextIndex = 1;
 
+  function changeWord() {
+    $(words[currentIndex]).removeClass('animate__fadeIn').addClass('animate__fadeOut');
+    $(words[nextIndex]).removeClass('animate__fadeOut').addClass('animate__fadeIn');
+
+    currentIndex = nextIndex;
+    nextIndex = (nextIndex + 1) % words.length;
+  }
+
+  setInterval(changeWord, 3000);
+});

9b6380e54ac090df215d919eca8945a07fd78083
BoyouChen <114955802+BoyouChen@users.noreply.github.com>
2023-03-27 17:19:06 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
       });
     }
   });
+
+  // Function to check if an element is in the viewport
+  function isInViewport(element) {
+    var elementTop = $(element).offset().top;
+    var elementBottom = elementTop + $(element).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  }
+
+  // Fade in content sections when they appear in the viewport
+  function fadeInContent() {
+    $('.hidden').each(function() {
+      if (isInViewport(this)) {
+        $(this).fadeIn(1000).removeClass('hidden');
+      }
+    });
+  }
+
+  // Initial check for content sections in the viewport
+  fadeInContent();
+
+  // Listen for scroll events and trigger fadeInContent()
+  $(window).scroll(fadeInContent);
 });

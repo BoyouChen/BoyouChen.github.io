9d45c27309fb36d2bfdec8baf0248e0cba8509f7
BoyouChen <114955802+BoyouChen@users.noreply.github.com>
2023-03-27 14:37:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 // Add smooth scrolling to all links
 $('a').on('click', function(event) {
-	if (this.hash !== '') {
-		event.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(hash).offset().top
-		}, 800, function(){
-			window.location.hash = hash;
-		});
-	}
+  if (this.hash !== '') {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
 });
+

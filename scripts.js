33c81aecabd78e9d499f86439eaa44ebe4ccea9b
BoyouChen <114955802+BoyouChen@users.noreply.github.com>
2023-05-08 19:11:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -51,35 +51,37 @@ function fadeInContent() {
   $(window).scroll(fadeInContent);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const words = ["Safety", "Efficiency", "Automation"];
-  const animatedText = document.getElementById("animated-text");
-  let currentWordIndex = 0;
-  let currentLetterIndex = 0;
-  let currentWord = words[currentWordIndex];
-  let animationInterval;
-
-  function animateText() {
-    let scrambledText = "";
-    for (let i = 0; i < currentWord.length; i++) {
-      if (i <= currentLetterIndex) {
-        scrambledText += currentWord[i];
+(function() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const words = ["Safety", "Efficiency", "Automation"];
+    const animatedText = document.getElementById("animated-text");
+    let currentWordIndex = 0;
+    let currentLetterIndex = 0;
+    let currentWord = words[currentWordIndex];
+    let animationInterval;
+  
+    function animateText() {
+      let scrambledText = "";
+      for (let i = 0; i < currentWord.length; i++) {
+        if (i <= currentLetterIndex) {
+          scrambledText += currentWord[i];
+        } else {
+          scrambledText += String.fromCharCode(
+            Math.floor(Math.random() * (126 - 33) + 33)
+          );
+        }
+      }
+      animatedText.textContent = scrambledText;
+  
+      if (currentLetterIndex < currentWord.length) {
+        currentLetterIndex++;
       } else {
-        scrambledText += String.fromCharCode(
-          Math.floor(Math.random() * (126 - 33) + 33)
-        );
+        currentLetterIndex = 0;
+        currentWordIndex = (currentWordIndex + 1) % words.length;
+        currentWord = words[currentWordIndex];
       }
     }
-    animatedText.textContent = scrambledText;
-
-    if (currentLetterIndex < currentWord.length) {
-      currentLetterIndex++;
-    } else {
-      currentLetterIndex = 0;
-      currentWordIndex = (currentWordIndex + 1) % words.length;
-      currentWord = words[currentWordIndex];
-    }
-  }
-
-  animationInterval = setInterval(animateText, 100);
-});
+  
+    animationInterval = setInterval(animateText, 100);
+  });
+})();
